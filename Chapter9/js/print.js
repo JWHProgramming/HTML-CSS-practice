f53a91fe75acbedbf4c3bffe85a35822949b3e68
Jeffrey Hendricks <jwhprogramming@gmail.com>
2020-09-23 12:46:04 -0700

    Chapter 15 almost complete

---
+++ Chapter9/js/print.js
@@ -0,0 +1,11 @@
+var $ = function(id){
+    return document.getElementById(id);
+}
+
+var printPage = function(){
+    window.print();
+}
+
+window.onload = function(){
+    $("printButton").onclick = printPage;
+}
